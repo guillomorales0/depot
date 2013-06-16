93d88a541f7455be7b21419c4b81410ff32371f1
Guillermo <guillomorales0@gmail.com>
2013-06-16 10:01:56 -0500

    Adding a column to an existing table, with a default value , Migrating existing data into the new table format, Using the logger to log events, Adjusting the way a table is rendered, using CSS

--- test/functional/carts_controller_test.rb
+++ test/functional/carts_controller_test.rb
@@ -1,3 +1,4 @@
+
 require 'test_helper'
 
 class CartsControllerTest < ActionController::TestCase
@@ -41,9 +42,10 @@ class CartsControllerTest < ActionController::TestCase
 
   test "should destroy cart" do
     assert_difference('Cart.count', -1) do
+      session[:cart_id] = @cart.id
       delete :destroy, id: @cart
     end
 
-    assert_redirected_to carts_path
+    assert_redirected_to store_path
   end
 end
